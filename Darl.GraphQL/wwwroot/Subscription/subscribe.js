7426185efd18aa3fb03a6df161cfb4063db0036f
Andrew Edmonds <sales@docandys.com>
2021-05-24 10:34:03 +0100

    All layouts the same content

--- Darl.GraphQL/wwwroot/Subscription/subscribe.js
+++ Darl.GraphQL/wwwroot/Subscription/subscribe.js
@@ -13,20 +13,6 @@ const elements = stripe.elements();
 cardElement = elements.create('card');
 cardElement.mount('#card-element');
 
-// This sample only supports a Subscription with payment
-// upfront. If you offer a trial on your subscription, then
-// instead of confirming the subscription's latest_invoice's
-// payment_intent. You'll use stripe.confirmCardSetup to confirm
-// the subscription's pending_setup_intent.
-// See https://stripe.com/docs/billing/subscriptions/trials
-
-// Payment info collection and confirmation
-// When the submit button is pressed, attempt to confirm the payment intent
-// with the information input into the card element form.
-// - handle payment errors by displaying an alert. The customer can update
-//   the payment information and try again
-// - Stripe Elements automatically handles next actions like 3DSecure that are required for SCA
-// - Complete the subscription flow when the payment succeeds
 const form = document.querySelector('#subscribe-form');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -45,8 +31,8 @@ form.addEventListener('submit', async (e) => {
             setMessage(`Payment failed: ${result.error.message}`);
         } else {
             // Redirect the customer to their account page
-            setMessage('Success! Redirecting to your account.');
-            window.location.href = '/account.html';
+            setMessage('Success! Redirecting to ThinkBase.');
+            window.location.href = '/';
         }
     });
 });
